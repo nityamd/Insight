7313423336f083c93a47191fbd5f81ce6ef3d370
nityamd <nityamd@gmail.com>
2018-10-02 15:35:47 -0400

    still week 4

--- my_flask_app/views.py
+++ my_flask_app/views.py
@@ -1,10 +1,8 @@
-from flaskexample import app
-
 from flask import request
 from flask import render_template
 from flask import json
 from flask import jsonify
-from flaskexample import app
+from my_flask_app import app
 #from sqlalchemy import create_engine
 #from sqlalchemy_utils import database_exists, create_database
 import pandas as pd
@@ -21,11 +19,11 @@ def result():
    if request.method == 'POST':
       result = request.form
       return render_template("result.html",result = result)
-@app.route('/cat',methods = ['POST', 'GET'])
-def cat():
+@app.route('/insight',methods = ['POST', 'GET'])
+def insight():
    if request.method == 'POST':
-      cat = request.form
-      return render_template("cat.html",result = result)
+      insight = request.form
+      return render_template("insight.html",result = result)
 
 if __name__ == '__main__':
    app.run(debug = True)
--- run.py
+++ run.py
@@ -1,3 +1,3 @@
 #!/usr/bin/env python
-from flaskexample import app
+from my_flask_app import app
 app.run(debug = True)
