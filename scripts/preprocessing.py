ae62d18fc5650ed913fe7f709bc5fc28173d60e4
nityamd <nityamd@gmail.com>
2018-10-03 20:53:32 -0400

    thingz

---
+++ scripts/preprocessing.py
@@ -0,0 +1,22 @@
+
+import pandas as pd
+import numpy as np
+import requests
+import os
+
+def sanitize_characters(raw, clean):
+    for line in input_file:
+        out = line
+        output_file.write(line)
+
+
+sanitize_characters(input_file, output_file)
+
+def standardize_text(df, text_field):
+    df[text_field] = df[text_field].str.replace(r"http\S+", "")
+    df[text_field] = df[text_field].str.replace(r"http", "")
+    df[text_field] = df[text_field].str.replace(r"@\S+", "")
+    df[text_field] = df[text_field].str.replace(r"[^A-Za-z0-9(),!?@\'\`\"\_\n]", " ")
+    df[text_field] = df[text_field].str.replace(r"@", "at")
+    df[text_field] = df[text_field].str.lower()
+    return df
